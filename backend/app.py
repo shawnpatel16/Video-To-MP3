11d39187574561a2ab4bc23b34e8260b6765d5c8
Shawn Patel <91225387+shawnpatel16@users.noreply.github.com>
2023-06-08 15:49:01 -0700

    adding instant download upon conversion functionality. Download name based on youtube video title name

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from pytube import YouTube
 from moviepy.editor import VideoFileClip, concatenate_videoclips
-
+import re
 app = Flask(__name__)
-CORS(app)  # Enable CORS
+CORS(app, expose_headers=["Content-Disposition"])
 
 @app.route('/')
 def hello_world():
@@ -14,27 +14,21 @@ def hello_world():
 def convert_video():
     video_url = ''
     data = request.get_json()
-    print(data)
     video_url = data['youtubeLink']
-    print(video_url)
     timestamps = data['timestamps']
-    print(timestamps)
     timestamps_in_seconds = []
     for timestamp in timestamps:
         timestamps_in_seconds.append(convert_to_seconds(timestamp['start'], timestamp['end']))
-    print(timestamps_in_seconds)
-    file_path = download_video(video_url)
-
-    edit_video(file_path,timestamps_in_seconds)
-    
-    return jsonify(data)
+    file_path,video_title = download_video(video_url)
+    audio_path, video_title = edit_video(file_path,timestamps_in_seconds,video_title)
+    return send_file(audio_path,mimetype='audio/mpeg',as_attachment=True, download_name=f"{video_title}")
 
 def download_video(url):
     youtube = YouTube(url)
     video = youtube.streams.get_highest_resolution()
     file_path = video.download('./sample_videos')
     print("great success")
-    return file_path
+    return file_path, youtube.title
 
 def convert_to_seconds(start,end):
     start_minutes, start_seconds = map(int, start.split(":"))
@@ -43,7 +37,8 @@ def convert_to_seconds(start,end):
     total_end_seconds = end_minutes * 60 + end_seconds
     return [total_start_seconds,total_end_seconds]
 
-def edit_video(video_path, timestamps):
+def edit_video(video_path, timestamps, video_title):
+    video_title = re.sub(r'[\/:*?"<>|]', '', video_title)
     clip = VideoFileClip(video_path)
 
     subclips = []
@@ -58,10 +53,11 @@ def edit_video(video_path, timestamps):
     final_clip = concatenate_videoclips(subclips)
     final_audio_clip = final_clip.audio
     final_audio_clip.fps = 44100
-    audio_path = video_path.replace('.mp4', '.mp3')
+    audio_path = f"./sample_videos/{video_title}.mp3"
     final_audio_clip.write_audiofile(audio_path)
-    return audio_path
+    return audio_path, video_title
     print("another great success")
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True, port = 8000)
--- frontend/src/components/VideoForm.vue
+++ frontend/src/components/VideoForm.vue
@@ -9,7 +9,7 @@
 
     <button @click="addTimestamp">Add More Timestamps</button>
 
-    <button @click="submit">Submit</button>
+    <button @click="convertAndDownload">Submit</button>
   </div>
 </template>
 
@@ -27,12 +27,30 @@ export default {
     addTimestamp() {
       this.timestamps.push({ start: '', end: '' })
     },
-    async submit() {
-      const response = await axios.post('http://localhost:8000/api/convert', {
-        youtubeLink: this.youtubeLink,
-        timestamps: this.timestamps
-      })
-      console.log(response)
+    convertAndDownload() {
+        axios({
+            url: 'http://localhost:8000/api/convert',
+            method: 'POST',
+            data: {
+                youtubeLink: this.youtubeLink,
+                timestamps: this.timestamps,
+            },
+            responseType: 'blob', 
+        }).then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+          link.href = url;
+          const contentDisposition = String(response.headers['content-disposition']);
+            let fileName = 'download.mp3';  
+            if (contentDisposition) {
+              const fileNameMatch = contentDisposition.match(/attachment; filename=(.+)/);
+                if (fileNameMatch.length === 2)
+                    fileName = `${fileNameMatch[1]}.mp3`;
+            }
+            link.setAttribute('download', fileName);
+            document.body.appendChild(link);
+            link.click();
+        });
     }
   }
 }
